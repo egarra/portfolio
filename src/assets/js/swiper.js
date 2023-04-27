de3e7726e6c90a63db5d3adbb8e319a631458810
egarra <garikhabuzov@gmail.com>
2023-04-27 18:25:49 +0300

    slider add

---
+++ src/assets/js/swiper.js
@@ -0,0 +1,9 @@
+$('.projects__swiper').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+    nextArrow: '.projects__next',
+    prevArrow: '.projects__prev',
+  });
